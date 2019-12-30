e221458816649ee854e2bdc09e5bde6299992832
khalil192 <shaikkhalil192@gmail.com>
2019-12-30 13:21:11 +0530

    sort with color visualizations

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'wait.dart';
 
-int numBars = 30;
+int numBars = 50;
 
 void main() {
   runApp(MyApp());
@@ -52,13 +52,13 @@ class BarRow extends StatelessWidget {
     //   },
     // );
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
         for(int i=0;i<valueController.numBars;i++) ValueListenableBuilder(
           valueListenable: valueController.valueNotifiers[i],
           builder: (context,value,child){
-            return Bar(valueController.values[i]);
+            return Bar(valueController.valueNotifiers[i],valueController.changeNotifiers[i]);
           },
         )
       
@@ -71,9 +71,11 @@ class BarRow extends StatelessWidget {
 
 class Bar extends StatefulWidget {
   double height;
-  double indi;
+  int isChanged;
+  ValueNotifier<double> val;
+  ValueNotifier<int>change;
   Color barColor = Colors.blue;
-  Bar(this.height){
+  Bar(this.val,this.change){
     // barColor = Colors.blue;
   }
   @override
@@ -81,38 +83,48 @@ class Bar extends StatefulWidget {
 }
 
 class _BarState extends State<Bar> {
+  Color findColor(int num){
+    Color currColor = widget.change.value ==0 ? Colors.blue : Colors.black;
+    if(widget.change.value ==2){
+      currColor = Colors.green;
+    }
+    return currColor;
+  }
   @override
   Widget build(BuildContext context) {
     Color currColor = Colors.blue;
-    currColor = widget.barColor;
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: SizedBox(
-        width: 10,
-        height: widget.height*500,
-        child: Container(
-          color: currColor,
-        ),
-      ),
+    return ValueListenableBuilder(
+      valueListenable: widget.change,
+      builder: (context,value,child){ 
+          return Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: SizedBox(
+          width: 2,
+          height: widget.val.value*500,
+          child: Container(
+            color: findColor(value),
+          ),
+        )
+        );
+    }
     );
-  }
 }
-
+}
 
 class ValueController {
   int numBars;
   List<double> values;
   List<ValueNotifier<double>> valueNotifiers;
-  // List<ValueNotifier<Color>>colorNotifiers;
+  List<ValueNotifier<int>>changeNotifiers;
   ValueController(this.numBars){
     var randomNumberGenerator = new Random();
     values = new List<double>(numBars);
-    // colorNotifiers = new List<ValueNotifier<Color>>(numBars);
+    changeNotifiers = new List<ValueNotifier<int>>(numBars);
     valueNotifiers = new List<ValueNotifier<double>> (numBars);
     for(int i=0;i<numBars;i++){
       values[i]  = randomNumberGenerator.nextDouble();
       valueNotifiers[i] = new ValueNotifier(values[i]);
-      // colorNotifiers[i] = new ValueNotifier(Colors.blue);
+      changeNotifiers[i] = new ValueNotifier(0);
     }
   }
   void swapValues(int index1,int index2) async{ //swap values in valueNotifiers
@@ -131,12 +143,21 @@ class ValueController {
   void bubbleSort() async{
     for(int i = 0;i<numBars;i++){
       for(int j=0;j<numBars-1;j++){
-          if(values[j+1] > values[j]){
+          if(values[j+1] < values[j]){
+            changeNotifiers[j].value = 1;
+            changeNotifiers[j+1].value = 1;
             swapValues(j+1,j);
             await wait();
+            changeNotifiers[j].value = 0;
+            changeNotifiers[j+1].value = 0;
+            await wait();
+            // print('swapped');
+            // print(j);
             }
-          // valueNotifiers[]
       }
+        // print('changed color');
+        // print(numBars-i-1);
+        changeNotifiers[numBars-i-1].value = 2;
     }
   }
   // void selectionSort() async{
--- lib/wait.dart
+++ lib/wait.dart
@@ -4,7 +4,7 @@ Future sleepSum(int valueOne, int valueTwo) {
   return Future.delayed(const Duration(seconds: 1), () => valueOne + valueTwo);
 }
 
-Future wait({double speed = 0.75}) {
-  final milliseconds = lerpDouble(100, 1000, speed).toInt();
+Future wait({double speed = 0.9}) {
+  final milliseconds = lerpDouble(100, 1, speed).toInt();
   return Future.delayed(Duration(milliseconds: milliseconds));
 }
